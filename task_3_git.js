bf0cd6e7f4e8c13b53f5e767aafffb5575acf82e
itsmeharikrishnanv <itsmeharikrishnanv@gmail.com>
2023-11-25 13:07:24 +0530

    task_3_style_change

---
+++ task_3_git.js
@@ -0,0 +1,11 @@
+let title = document.all[10];
+title.textContent = "Hai its Hari";
+
+let headerElement = document.getElementById("main-header");
+headerElement.style.border = "solid 3px black";
+
+let addHeading = document.getElementById("add-items");
+//console.log(addHeading);
+addHeading.style.color = "green";
+addHeading.style.fontWeight = "bold";
+
