9d9f7935dd9468c9c59a7790e7f3ff47ca36ce1d
itsmeharikrishnanv <itsmeharikrishnanv@gmail.com>
2023-11-26 08:54:35 +0530

    task4

---
+++ task4.js
@@ -0,0 +1,6 @@
+let listElements = document.getElementsByClassName("list-group-item");
+listElements[2].style.backgroundColor = "green";
+
+for (let i =0;i<listElements.length;i++){
+    listElements[i].style.fontWeight = "bold";
+}
