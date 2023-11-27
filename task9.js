6fa08305d0f75058178517e4c66fa7860a3a697d
itsmeharikrishnanv <itsmeharikrishnanv@gmail.com>
2023-11-27 12:58:30 +0530

    task 9 code push

---
+++ task9.js
@@ -0,0 +1,133 @@
+
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+let inputOne = document.getElementById("item");
+let inputTwo = document.getElementById("item-description");
+
+let labelOne = document.createElement("label");
+labelOne.textContent = "Item Number";
+labelOne.setAttribute("for","item");
+form.insertBefore(labelOne,inputOne);
+
+
+let labelTwo = document.createElement("label");
+labelTwo.textContent = "Item Description";
+labelTwo.setAttribute("for","item-description");
+form.insertBefore(labelTwo,inputTwo);
+
+
+
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
+
+// Add item
+function addItem(e){
+  e.preventDefault();
+
+  // Get input value
+  var newItem = document.getElementById('item').value;
+  let newItemDesc = document.getElementById('item-description').value;
+  let newText = newItem +" "+ newItemDesc;
+
+  // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'list-group-item';
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newText));
+
+  // Create del button element
+  var deleteBtn = document.createElement('button');
+
+  // Add classes to del button
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  // Append button to li
+  li.appendChild(deleteBtn);
+
+  let editButtonele = document.createElement("button");
+  editButtonele.className = 'btn btn-danger btn-sm float-right delete';
+  let textOfEdit = document.createTextNode("Edit");
+  editButtonele.appendChild(textOfEdit);
+  li.appendChild(editButtonele);
+
+
+  // Append li to list
+  itemList.appendChild(li);
+}
+
+// Remove item
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
+
+// Filter Items
+function filterItems(e){
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  // Get lis
+  var items = itemList.getElementsByTagName('li');
+  // Convert to an array
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
+
+
+//Adding edit button
+
+let editButtonArray = [];
+
+let editButtonele1 = document.createElement("button");
+editButtonele1.className = 'btn btn-danger btn-sm float-right delete';
+let textOfEdit1 = document.createTextNode("Edit");
+editButtonele1.appendChild(textOfEdit1);
+editButtonArray.push(editButtonele1);
+
+
+
+let editButtonele2 = document.createElement("button");
+editButtonele2.className = 'btn btn-danger btn-sm float-right delete';
+let textOfEdit2 = document.createTextNode("Edit");
+editButtonele2.appendChild(textOfEdit2);
+editButtonArray.push(editButtonele2);
+
+
+let editButtonele3 = document.createElement("button");
+editButtonele3.className = 'btn btn-danger btn-sm float-right delete';
+let textOfEdit3 = document.createTextNode("Edit");
+editButtonele3.appendChild(textOfEdit3);
+editButtonArray.push(editButtonele3);
+
+
+let editButtonele4 = document.createElement("button");
+editButtonele4.className = 'btn btn-danger btn-sm float-right delete';
+let textOfEdit4 = document.createTextNode("Edit");
+editButtonele4.appendChild(textOfEdit4);
+editButtonArray.push(editButtonele4);
+
+
+//console.log(editButtonArray);
+
+let listElements = document.getElementsByClassName("list-group-item");
+for(let i=0;i<listElements.length;i++){
+    listElements[i].appendChild(editButtonArray[i]);
+}
